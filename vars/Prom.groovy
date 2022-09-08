37a9dd91f040d28e2c91ef4e6c83839666ffaf6d
Steve <9054646+stendur@users.noreply.github.com>
2022-09-08 13:52:47 -0400

    vars folder

--- vars/Prom.groovy
+++ vars/Prom.groovy
@@ -1,6 +1,7 @@
 def send_message(String url, String message) {
     def pushGateway = new URL(url).openConnection()
     pushGateway.setRequestMethod("POST")
+    pushGateway.setDoOutput(true);
     pushGateway.getOutputStream().write(msg.getBytes("UTF-8"))
     def postRC = pushGateway.getResponseCode()
     if (!postRC.equals(200)) {
